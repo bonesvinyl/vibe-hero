8ea9a911fe20b14970563a05f2bc915501f7421c
bonesvinyl <bonesvinyl@gmail.com>
2026-09-08 13:28:00 -0400

    Brand extension and website with Nightmare Hero VH icon

--- scripts/package-extension.js
+++ scripts/package-extension.js
@@ -8,3 +8,6 @@ for (const file of (await readdir("public/sounds")).filter(file => /\.(mp3|json)
 
 await mkdir("dist-extension/fonts", { recursive: true });
 for (const file of ["Nightmare_Hero_Normal.ttf", "BarlowCondensed-Medium.ttf", "BarlowCondensed-SemiBold.ttf", "Barlow-OFL.txt", "SOURCE.txt"]) await copyFile(`public/fonts/${file}`, `dist-extension/fonts/${file}`);
+
+await mkdir('dist-extension/icons', { recursive: true });
+for (const size of [16,32,48,128]) await copyFile(`public/icons/vh-${size}.png`, `dist-extension/icons/vh-${size}.png`);
